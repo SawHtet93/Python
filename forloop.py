995b98fe89e1b6084c179feae9be110d3ef09881
Saw Htet Khine Soe <sawhtetkhinesoe@gmail.com>
2018-08-07 17:25:23 +0630

    Create forloop.py

---
+++ forloop.py
@@ -0,0 +1,17 @@
+'''    for i in range (5):
+	      print("Saw Htet")     '''
+  
+  ''' for i in range (5):
+	      print(i)   '''
+  
+  '''  name = "saw htet"
+  for i in range(5):
+	  print (name)  '''
+    
+    '''  for i in range(1,5):
+	print(i)  '''
+  
+  num = 0
+ for i in range(5):
+	num = num + i
+	print(num)
