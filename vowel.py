6a6a3740c01300cc960ce023419ae735a63a668d
Saw Htet Khine Soe <sawhtetkhinesoe@gmail.com>
2018-08-07 16:57:48 +0630

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,6 @@
+user = input("Enter any alphabe: ")
+if ((user =='a') | (user =='e') | (user =='i') | (user =='o') | (user =='u')):
+    print ("The alphabe is vowel:",user)
+else:
+    print ("the alphabet is consonant:",user)
+
