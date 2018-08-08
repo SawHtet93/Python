ae565b6c2de7441849708dbb7bb3d0b2afbb42f7
Saw Htet Khine Soe <sawhtetkhinesoe@gmail.com>
2018-08-08 22:13:49 +0630

    Update Program_to_print_factorial_of_number.py

--- Program_to_print_factorial_of_number.py
+++ Program_to_print_factorial_of_number.py
@@ -1,5 +1,15 @@
-fact = 1
+''' fact = 1
 num = int(input("Enter any Number: "))
 for i in range (1,num+1):
     fact = fact * i
-print(fact)
+print(fact)'''
+
+
+fact = 1
+num = int(input("Enter any Number: "))
+i = 1
+while(i<num):
+    fact = fact * i
+    i = i + 1
+print ("the factor of",num , "is ",fact)
+
