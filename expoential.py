afdedd56e037ee67995e154340841f5b1bf5c219
Saw Htet Khine Soe <sawhtetkhinesoe@gmail.com>
2018-08-11 14:17:51 +0630

    Create expoential.py

---
+++ expoential.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: "))
+exp = int(input("Enter exponential number: "))
+result = 1
+for i in range(1,(exp+1)):
+    result = result * num
+print("The result is: ",result)
