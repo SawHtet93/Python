48e771c182f92c31e609abcea1b740d2170e03ab
Saw Htet Khine Soe <sawhtetkhinesoe@gmail.com>
2018-08-11 23:05:37 +0630

    Update break.py

--- break.py
+++ break.py
@@ -1,6 +1,12 @@
-i = 1
+"""i = 1
 while(i<=100):
     print(i)
     if (i==50):
         break
     i = i+1
+"""
+
+while 1:
+    name= input("Enter the name: ")
+    if (name == 'quick'):
+        break
