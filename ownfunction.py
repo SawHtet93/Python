9ad566ead7b02d563cef38acc11d0d920fcb27fa
Saw Htet Khine Soe <sawhtetkhinesoe@gmail.com>
2018-08-13 00:00:53 +0630

    Create ownfunction.py

---
+++ ownfunction.py
@@ -0,0 +1,11 @@
+#def defind for function
+def example():
+    print("Welcome to my function")
+example()
+
+def add():
+    n1 = int(input("Enter first number:"))
+    n2 = int(input("Enter second number:"))
+    n3 = n1+n2
+    print(n3)
+add()
