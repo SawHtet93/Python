a082592a17e154bea763821e8b06eabe3436d89e
Saw Htet Khine Soe <sawhtetkhinesoe@gmail.com>
2018-08-14 13:55:59 +0630

    Create list.py

---
+++ list.py
@@ -0,0 +1,14 @@
+list1 = [23,34,35,27]
+print list1
+list1[0]
+print len(list1)
+list1[2]=500
+print list1[1:3]
+list2 = ['peter',john','robert']
+print list2
+print list2[1] = "williams"
+print list2
+print list2[-1]
+
+liar3 = ['peter','john',1947,33]
+print list3
