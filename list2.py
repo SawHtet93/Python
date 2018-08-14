fdf62c35c348741e0e789470bb8c1f42193d2005
Saw Htet Khine Soe <sawhtetkhinesoe@gmail.com>
2018-08-14 14:17:15 +0630

    Create list2.py

---
+++ list2.py
@@ -0,0 +1,11 @@
+#list2
+1 = ['peter','jone',33,44,55,66]
+# 1 is object , object is can access method
+#eg 1.appent('williams')
+#appent is add end of array
+#1.index('jone') to take jone index number
+#1.remove('jone') for remove
+#1.reverse()  sort by reverse
+#1.pop()
+#1.count('peter')
+#dir()
