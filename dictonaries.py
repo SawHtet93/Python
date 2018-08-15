21b35be85cc9eb2c7edff7d8d56901244965de32
Saw Htet Khine Soe <sawhtetkhinesoe@gmail.com>
2018-08-15 10:30:37 +0630

    Create dictonaries.py

---
+++ dictonaries.py
@@ -0,0 +1,14 @@
+age = {'john':21,'peter':35,'bob':44}  #key and value
+
+#age
+#print(age)
+#age["john"]
+#age["williams"] = 55
+
+dict1 = {'john':"married",'peter':23,24:1024}
+a = [1,2,3,4,5,6]
+b = [6,7,8,9]
+dict2 = {'a':[1,2,3,4,5],'b':[6,7,8,9]}
+#dict2
+#del age['bob']
+#age.
