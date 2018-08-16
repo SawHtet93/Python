1ea92ce59a4fbcc04299a5fe765546849f63d288
Saw Htet Khine Soe <sawhtetkhinesoe@gmail.com>
2018-08-16 11:04:53 +0630

    Update timeModule.py

--- timeModule.py
+++ timeModule.py
@@ -7,8 +7,11 @@ time.sleep(3)
 print("image2")
 time.sleep(3)
 
+
+#time delay
 for a in range(1,11):
     print('\n')
+    input("")  #Small Assiangment
     time.sleep(3)
     for b in range(1,11):
         print(a,"x",b,"=",a*b)
