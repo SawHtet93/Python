ab54bcffd361e698d42091fe1353a252f188c1dc
Saw Htet Khine Soe <sawhtetkhinesoe@gmail.com>
2018-08-16 10:58:08 +0630

    Update timeModule.py

--- timeModule.py
+++ timeModule.py
@@ -2,3 +2,13 @@ import time
 #time.time()
 #t = time.asctime()
 #print(t)
+print("image1")
+time.sleep(3)
+print("image2")
+time.sleep(3)
+
+for a in range(1,11):
+    print('\n')
+    time.sleep(3)
+    for b in range(1,11):
+        print(a,"x",b,"=",a*b)
