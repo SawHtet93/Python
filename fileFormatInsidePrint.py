3e5b7b9e970137e8d1f1ecc923d420263f038b47
Saw Htet Khine Soe <sawhtetkhinesoe@gmail.com>
2018-08-17 13:02:17 +0630

    Create fileFormatInsidePrint.py

---
+++ fileFormatInsidePrint.py
@@ -0,0 +1,9 @@
+name = "saw"
+age = 35
+per = 65.9
+
+#print("The name is",name,"the age is",age,"persentage is ",per)
+print("The name is %s the age is %s persentage is %s" %(name,age,per))
+print("The name is %s the age is %f persentage is %f" %(name,age,per))
+
+#s is string .2f is float d is integer
