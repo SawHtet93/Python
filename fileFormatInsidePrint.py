4f2d3d2d595b32b832005517eed27158c01783d9
Saw Htet Khine Soe <sawhtetkhinesoe@gmail.com>
2018-08-17 14:33:00 +0630

    Update fileFormatInsidePrint.py

--- fileFormatInsidePrint.py
+++ fileFormatInsidePrint.py
@@ -5,5 +5,7 @@ per = 65.9
 #print("The name is",name,"the age is",age,"persentage is ",per)
 print("The name is %s the age is %s persentage is %s" %(name,age,per))
 print("The name is %s the age is %f persentage is %f" %(name,age,per))
+print ('the name is {} and agie is {}'.format(name,age))
+#print ('the name is {0} and agie is {1}'.format(name,age))
 
 #s is string .2f is float d is integer
