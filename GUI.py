4afab4eb5021546aade4b41bcfe696f5eebc726b
Saw Htet Khine Soe <sawhtetkhinesoe@gmail.com>
2018-08-21 17:34:49 +0630

    Create GUI.py

---
+++ GUI.py
@@ -0,0 +1,12 @@
+""" #import calendar
+#print (calendar.month(2018,8))
+
+#from calendar import *
+#print(month(2018,8)
+
+#from math import *
+#pow(2,3)  """
+
+from tkinter import *
+a = Tk()
+a.title("My first windows")
